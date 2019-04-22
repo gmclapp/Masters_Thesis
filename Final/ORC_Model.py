c1852fcd92ddb36467955e2c05d280471a49b0bd
Glenn Clapp <glenn.m.clapp@gmail.com>
2019-04-22 05:25:49 -0400

    Work session

--- Final/ORC_Model.py
+++ Final/ORC_Model.py
@@ -14,19 +14,81 @@ def turbine_model():
     pass
 def pump_model():
     pass
-def ORC_model():
+def ORC_model(cond_pres, boil_pres, eff_t, eff_p):
     '''This function takes a working pressure for a condenser and boiler, an
     efficiency for a pump and turbine, and outputs the work per unit mass flow
     and thermal efficiency for a generic ORC, as well as the working temperature
     and enthalpy at each of the four fixed states which can then be used in the
     models for the individual major components.'''
-    pass
+
+    R245fa_db = '\\R245fa Saturated properties temperature table.csv'
+    db_path = r'C:\Users\Glenn Clapp\Documents\GitHub\Masters_Thesis\GHSP study\Additional references'
+
+    temp_col = 0 # Degrees Celsius
+    press_col = 1 # MPa
+    v_col = 3 # Specific volume of vapor m3/kg
+    hl_col = 4 # Enthalpy of saturated liquid kJ/kg
+    hv_col = 5 # Enthalpy of saturated vapor kJ/kg
+    sl_col = 6 # Entropy of saturated liquid kJ/(kgK)
+    sv_col = 7 # Entropy of saturated vapor kJ/(kgK)
+        
+    x1,x2,y1,y2 = mf.vlookup(db_path+R245fa_db,
+                             boil_pres, press_col, hv_col)
+    
+    h1 = mf.interpolate(x1,x2,y1,y2,boil_pres)
+    
+    x1,x2,y1,y2 = mf.vlookup(db_path+R245fa_db,
+                             boil_pres, press_col, sv_col)
+    
+    s1 = mf.interpolate(x1,x2,y1,y2,boil_pres)
+    s2 = s1 # This is the adiabatic volumetric assumption in the turbine
+
+    x1,x2,y1,y2 = mf.vlookup(db_path+R245fa_db,
+                             cond_pres, press_col, hl_col)
+    
+    h3 = mf.interpolate(x1,x2,y1,y2,cond_pres)
+    
+    x1,x2,y1,y2 = mf.vlookup(db_path+R245fa_db,
+                             cond_pres, press_col, sl_col)
+    
+    s3 = mf.interpolate(x1,x2,y1,y2,cond_pres)
+
+    # get h3 enthalpy of a saturated liquid at condenser pressure
+    # get s3 entropy of a saturated vapor at condenser pressure
+
+    # determine if the working fluid is wet, dry, or adiabatic.
+
+    x1,x2,y1,y2 = mf.vlookup(db_path+R245fa_db,
+                                   cond_pres, press_col, sl_col)
+
+    s2f = mf.interpolate(x1,x2,y1,y2,cond_pres)
+    x1,x2,y1,y2 = mf.vlookup(db_path+R245fa_db,
+                                   cond_pres, press_col, sv_col)
+
+    s2g = mf.interpolate(x1,x2,y1,y2,cond_pres)
+
+    quality = (s2 - s2f)/(s2g - s2f)
+    h2f = h3
+
+    
+    # h2g = enthalpy at state 2 for a saturated liquid
+    h2fg = h2g-h2f
+    h2s = h2f + quality* h2fg
+    h2 = h1 - eff_t*(h1 - h2s)
+    
 
 #------Main------#
 if __name__ == '__main__':
     '''Manual entry begins here.'''
     condenser_pressure = si.get_real_number("Enter condenser pressure.\n>>>")
     boiler_pressure = si.get_real_number("Enter boiler pressure.\n>>>")
-    ORC_model()
+    turbine_efficiency = si.get_real_number("Enter the turbine efficiency (0-1).\n>>>",
+                                            upper=1.0,lower=0)
+    pump_efficiency = si.get_real_number("Enter the pump efficiency (0-1).\n>>>",
+                                         upper=1.0, lower=0)
+    ORC_model(condenser_pressure,
+              boiler_pressure,
+              turbine_efficiency,
+              pump_efficiency)
 else:
     pass
